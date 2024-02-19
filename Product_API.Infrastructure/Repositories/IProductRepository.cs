42bd022acebe87227fc3fdee2feaad17caedcea9
Dxrk <AdidasU.F20@gmail.com>
2024-02-19 10:54:51 +0500

    changes

--- Product_API.Infrastructure/Repositories/IProductRepository.cs
+++ Product_API.Infrastructure/Repositories/IProductRepository.cs
@@ -1,10 +1,53 @@
-﻿using Product.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Product_API.Data;
+using Product_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Product_API.Infrastructure.Repositories
 {
-    public interface IProductRepository
+    public class ProductPostgressRepository : IProductPostgressRepository
     {
-        ProductModel Add(ProductModel product);
-        List<ProductModel> GetAll();
+        private readonly ProductDbContext _context;
+
+        public ProductPostgressRepository(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task Update(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var productToDelete = await _context.Products.FindAsync(id);
+            if (productToDelete != null)
+            {
+                _context.Products.Remove(productToDelete);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
